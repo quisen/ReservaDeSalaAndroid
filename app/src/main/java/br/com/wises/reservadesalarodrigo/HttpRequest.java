c8e5308921e5a6269743368e4c21ba52d8e5c1cb
quisen <quisen@alunos.utfpr.edu.br>
2020-02-13 00:12:35 -0300

    Melhorias na UI

--- app/src/main/java/br/com/wises/reservadesalarodrigo/HttpRequest.java
+++ app/src/main/java/br/com/wises/reservadesalarodrigo/HttpRequest.java
@@ -15,66 +15,70 @@ import java.util.Map;
 
 public class HttpRequest {
 
-	Context context;
-	Map<String, String> headers;
-	String url;
-	int reqMethod;
+    Context context;
+    Map<String, String> headers;
+    String url;
+    int reqMethod;
 
-	public HttpRequest(Context c, Map<String, String> h, String u, String reqMethod) {
-		this.context = c;
-		this.headers = h;
-		this.url = u;
-		switch (reqMethod) {
-			case "GET":
-				this.reqMethod = Request.Method.GET;
-				break;
-			case "POST":
-				this.reqMethod = Request.Method.POST;
-				break;
-			case "PUT":
-				this.reqMethod = Request.Method.PUT;
-				break;
-			case "DELETE":
-				this.reqMethod = Request.Method.DELETE;
-				break;
-		}
-	}
+    public HttpRequest(Context c, Map<String, String> h, String u, String reqMethod) {
+        this.context = c;
+        this.headers = h;
+        this.url = u;
+        switch (reqMethod) {
+            case "GET":
+                this.reqMethod = Request.Method.GET;
+                break;
+            case "POST":
+                this.reqMethod = Request.Method.POST;
+                break;
+            case "PUT":
+                this.reqMethod = Request.Method.PUT;
+                break;
+            case "DELETE":
+                this.reqMethod = Request.Method.DELETE;
+                break;
+        }
+    }
 
-	public void doRequest() {
-		try {
-			// Formulate the request and handle the response.
-			StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-					new Response.Listener<String>() {
-						@Override
-						public void onResponse(String response) {
-							EventBus.getDefault().post(new Event("RequestSuccess", response));
-						}
-					},
-					new Response.ErrorListener() {
-						@Override
-						public void onErrorResponse(VolleyError error) {
-							try {
-								String errorResult = new String(error.networkResponse.data, "UTF-8");
-								EventBus.getDefault().post(new Event("RequestError", errorResult));
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						}
-					}) {
-				@Override
-				public Map<String, String> getHeaders() throws AuthFailureError {
-					return headers;
-				}
-			};
+    public void doRequest() {
+        try {
+            // Formulate the request and handle the response.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            EventBus.getDefault().post(new Event("RequestSuccess", response));
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            try {
+                                if (error.networkResponse != null) {
+                                    String errorResult = new String(error.networkResponse.data, "UTF-8");
+                                    EventBus.getDefault().post(new Event("RequestError", errorResult));
+                                }else {
+                                    EventBus.getDefault().post(new Event("RequestError", "Sem resposta do servidor"));
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    return headers;
+                }
+            };
 
-			stringRequest.setRetryPolicy(new DefaultRetryPolicy(
-					5000,
-					DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-					DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-			VolleySingleton.getInstance(context).addToRequestQueue(stringRequest);
+            stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    5000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            VolleySingleton.getInstance(context).addToRequestQueue(stringRequest);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
